cf2079aee97cd577c0ee85c25ff53bcf8be2d220
NikhilManohar86 <manohar.nikhil@gmail.com>
2020-02-10 13:09:53 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component'
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    // otherwise redirect to home
+    { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeaderComponent ],
+  declarations: [ AppComponent, HeaderComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
